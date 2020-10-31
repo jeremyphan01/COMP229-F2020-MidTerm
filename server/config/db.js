011032f184e9c3fad9f01ba09ba4fc64baaeeac0
jeremyphan01 <phankhanhhung1718@gmail.com>
2020-10-30 22:32:10 -0400

    first commit

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //local MongoDB deployment ->
+  "URI": "mongodb://localhost/books229",
+  "URI": "mongodb+srv://KhanhHung:tfkf2QkUaRPJpP7t@mongodbserver.peonh.mongodb.net/books229?retryWrites=true&w=majority",
+};
